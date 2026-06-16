b563ca0a184aece3557e34b20eb7122375febbba
seydx <hi@seydx.dev>
2026-06-17 00:40:54 +0200

    Update dev scripts

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -49,6 +49,12 @@ const directories = [
   '../camera-ui-tuya',
   '../camera-ui-wasm-motion',
   '../camera-ui-wyze',
+  '../mockups/camera-ui-mock-motion',
+  '../mockups/camera-ui-mock-object',
+  '../mockups/camera-ui-mock-tuya',
+  '../mockups/camera-ui-test-node',
+  '../mockups/camera-ui-test-go',
+  '../mockups/camera-ui-test-python',
 ];
 
 runCommandsInDirectories(directories);
