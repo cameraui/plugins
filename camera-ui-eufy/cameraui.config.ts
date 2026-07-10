d6801f3c8e7b606797f9f3c2be31e70ddb56bb61
seydx <hi@seydx.dev>
2026-07-10 02:33:17 +0200

    eufy: update build config and deps

--- camera-ui-eufy/cameraui.config.ts
+++ camera-ui-eufy/cameraui.config.ts
@@ -5,8 +5,13 @@ const mode = process.env.MODE || 'production';
 const config: CameraUiBuildOptions = {
   input: ['src/index.ts'],
   mode: mode === 'development' ? 'development' : 'production',
-  external: ['eufy-security-client'],
-  additionalFiles: [],
+  external: [],
+  additionalFiles: [
+    { source: '../externals/eufy-security-client/src/push/proto/mcs.proto', target: 'proto/mcs.proto' },
+    { source: '../externals/eufy-security-client/src/push/proto/checkin.proto', target: 'proto/checkin.proto' },
+    { source: '../externals/eufy-security-client/src/mqtt/proto/lock.proto', target: 'proto/lock.proto' },
+    { source: '../externals/eufy-security-client/src/mqtt/mqtt-eufy.crt', target: 'mqtt-eufy.crt' },
+  ],
 };
 
 export default config;
